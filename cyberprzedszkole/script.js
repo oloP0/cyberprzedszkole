a09fb455dc8ac0b6d24013ba81ee8f25edf1db5c
oloP0 <150745432+oloP0@users.noreply.github.com>
2026-03-03 22:48:31 +0100

    Update script.js

--- cyberprzedszkole/script.js
+++ cyberprzedszkole/script.js
@@ -231,3 +231,14 @@ window.addEventListener('keydown',(e)=>{
         showTestimonial(testimonialIndex);
     }
 });
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+if(hamburger){
+    hamburger.addEventListener('click', ()=>{
+        navList.classList.toggle('open');
+        hamburger.classList.toggle('active');
+    });
+}
+
